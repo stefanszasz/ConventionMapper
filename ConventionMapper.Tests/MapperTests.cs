252eb27255d6417ae9324ada86077ab0377a472b
Stefan Szasz <paichpresident@gmail.com>
2012-09-23 20:35:32 +0300

    Initial commit

---
+++ ConventionMapper.Tests/MapperTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace ConventionMapper.Tests
+{
+    public class MapperTests
+    {
+        [Fact]
+        public void MappingObject_WithSamePropertyTypeAndNames_ShouldMapTheValuesCorrectly_ByHavingTheResultObjectPropertyValues_EqualTo_TheSourcePropertyValues()
+        {
+            var customer = new Fixture().CreateAnonymous<Customer>();
+
+            CustomerDTO customerDTO = Mapper<Customer, CustomerDTO>.MapByPropertyConvention(customer);
+
+            Assert.Equal(customerDTO.Name, customer.Name);
+            Assert.Equal(customerDTO.Id, customer.Id);
+            Assert.Equal(customerDTO.LoginDate, customer.LoginDate);
+        }
+
+        [Fact]
+        public void MappingObject_WithSamePropertyTypeAndNames_ShouldFillTheResultPropertyValues_WithOnesFromSource_AndKeepTheExistingSetOnesIntact()
+        {
+            const string actual = "YO";
+
+            var customerDTO = new CustomerDTO { Id = actual };
+
+            var customer = new Fixture().CreateAnonymous<Customer>();
+
+            Mapper<CustomerDTO, Customer>.MapByPropertyConventionModifiyingExisting(customerDTO, customer);
+
+            Assert.Equal(customer.Name, customer.Name);
+            Assert.Equal(customer.Id, actual);
+            Assert.Equal(customer.LoginDate, customer.LoginDate);
+        }
+
+        public class Customer
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public DateTime LoginDate { get; set; }
+            public bool IsActive { get; set; }
+        }
+
+        public class CustomerDTO
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public DateTime LoginDate { get; set; }
+        }
+    }
+}
---
+++ ConventionMapper/Mapper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConventionMapper
+{
+    public static class Mapper<TSource, TOutput>
+        where TOutput : class
+        where TSource : class
+    {
+        [ThreadStatic]
+        private static TOutput _existingShape;
+
+        [ThreadStatic]
+        private static TSource _source;
+
+        public static TOutput MapByPropertyConvention(TSource source, Func<PropertyInfo, PropertyInfo, bool> propertyInfoConvention = null)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            _source = source;
+
+            var mappedResult = Activator.CreateInstance<TOutput>();
+
+            FillOutput(mappedResult, propertyInfoConvention);
+
+            return mappedResult;
+        }
+
+        public static void MapByPropertyConventionModifiyingExisting(TSource source, TOutput existingOutputToModify, Func<PropertyInfo, PropertyInfo, bool> propertyInfoConvention = null)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (existingOutputToModify == null) throw new ArgumentNullException("existingOutputToModify");
+
+            _source = source;
+            _existingShape = existingOutputToModify;
+
+            FillOutput(_existingShape, propertyInfoConvention);
+        }
+
+        private static void FillOutput(TOutput mappedResult, Func<PropertyInfo, PropertyInfo, bool> propertyInfoConvention)
+        {
+            if (propertyInfoConvention == null)
+                propertyInfoConvention = (sType, tType) => sType.PropertyType == tType.PropertyType;
+
+            foreach (var sourcePropertyInfo in typeof(TSource).GetProperties())
+            {
+                FillMatchedProperties(_source, sourcePropertyInfo, mappedResult, propertyInfoConvention);
+            }
+        }
+
+        private static void FillMatchedProperties(TSource source, PropertyInfo sourcePropertyInfo, TOutput result, Func<PropertyInfo, PropertyInfo, bool> propertyInfoConvention)
+        {
+            PropertyInfo targetPropertyInfo = typeof(TOutput).GetProperty(sourcePropertyInfo.Name,
+                                                                     BindingFlags.Instance | BindingFlags.Public |
+                                                                     BindingFlags.FlattenHierarchy |
+                                                                     BindingFlags.SetProperty);
+            if (targetPropertyInfo == null) return;
+
+            var sourceValue = sourcePropertyInfo.GetValue(source, null);
+
+            if (propertyInfoConvention(sourcePropertyInfo, targetPropertyInfo))
+            {
+                if (_existingShape != null)
+                {
+                    object targetValue = targetPropertyInfo.GetValue(_existingShape, null);
+                    if (sourceValue != targetValue)
+                    {
+                        targetPropertyInfo.SetValue(result, sourceValue ?? targetValue, null);
+                    }
+                }
+                else
+                {
+                    targetPropertyInfo.SetValue(result, sourceValue, null);
+                }
+            }
+        }
+    }
+
+}
